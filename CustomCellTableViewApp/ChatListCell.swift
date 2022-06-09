6f57901a13ca9e731f9223047a8cb86f4e6ab270
Pritam Dash <pritamdash2015@outlook.com>
2022-06-09 20:29:21 +0530

    UI of 1 type of cell complete.

---
+++ CustomCellTableViewApp/ChatListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatListCell.swift
+//  CustomCellTableViewApp
+//
+//  Created by Pritam Dash on 09/06/22.
+//
+
+import UIKit
+
+class ChatListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CustomCell-TableView-App
 iOS app containing a tableView with custom cells
+
+UI TableViewCell - Holds the UI of a cell of a tableView. We subclass this class and put design of cell in an .xib file
+The object of this UITableViewCell then becomes a part of the TableView.
